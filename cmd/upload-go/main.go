ee882743cf07d3fc10b3914af534de373b52ceb0
Jonas <jonas.cheng+trello@gmail.com>
2021-11-14 14:50:49 +0800

    feat(cmd): upload file with rate limit

--- cmd/upload-go/main.go
+++ cmd/upload-go/main.go
@@ -1,4 +1,95 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/juju/ratelimit"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	uploadServer = kingpin.Flag("server", "Upload server endpoint.").Default("http://localhost:8080/upload?token=secret").String()
+	srcFile      = kingpin.Flag("file", "Upload source file.").Default("./data/agent-telemetry-threat-info-flat-100-1636860627.json").String()
+	rateLimit    = kingpin.Flag("rate", "Upload rate limit in Mbps.").Default("100").Float64()
+)
+
+// wrapper to limit io.Reader speed
+func NewRateReader(reader io.Reader, ratelimitMbs float64) io.Reader {
+	NewRateReader := reader
+
+	if ratelimitMbs > 0 {
+		log.Printf("upload file in %vMbs\n", ratelimitMbs)
+		tokenPS := ratelimitMbs / 8 * 1024 * 1024
+		log.Printf("adding %v bucket tokens per second\n", tokenPS)
+		bucket := ratelimit.NewBucketWithRate(float64(tokenPS), int64(tokenPS))
+		NewRateReader = ratelimit.Reader(reader, bucket)
+	}
+
+	return NewRateReader
+}
+
+func UploadFile(server string, filePath string, ratelimitMbs float64) {
+	// create file reader
+	file, err := os.Open(filePath)
+	checkError(err)
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+
+	// create multipart writter
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile("file", fmt.Sprintf("%v-%v", time.Now().Unix(), filepath.Base(file.Name())))
+	checkError(err)
+	written, err := io.Copy(part, file)
+	checkError(err)
+	log.Printf("%v bytes has been copy to io.Writer\n", written)
+	writer.Close()
+
+	// create ratelimit reader
+	ratelimitBody := NewRateReader(body, ratelimitMbs)
+
+	log.Println("start http.NewRequest")
+	req, err := http.NewRequest("POST", server, ratelimitBody)
+	log.Println("end http.NewRequest")
+	checkError(err)
+
+	log.Println("start req.Header.Add")
+	req.Header.Add("Content-Type", writer.FormDataContentType())
+	log.Println("end req.Header.Add")
+
+	client := &http.Client{}
+	// transport := http.DefaultTransport.(*http.Transport)
+	// log.Printf("%v\n", transport)
+	// transport.WriteBufferSize = 1 * 100
+	// transport.ReadBufferSize = 1 * 100
+	// log.Printf("%v\n", transport)
+	// client.Transport = transport
+
+	log.Println("start client.Do")
+	resp, err := client.Do(req)
+	log.Println("end client.Do")
+	checkError(err)
+
+	fmt.Println(*resp)
+}
+
 func main() {
+	kingpin.Version("1.0.0")
+	kingpin.Parse()
+
+	UploadFile(*uploadServer, *srcFile, *rateLimit)
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -5,8 +5,7 @@ go 1.16
 require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20210927113745-59d0afb8317a // indirect
-	github.com/brianvoe/gofakeit/v6 v6.9.0
-	github.com/jszwec/csvutil v1.5.1
-	github.com/stretchr/testify v1.7.0
+	github.com/juju/ratelimit v1.0.1
+	github.com/stretchr/testify v1.7.0 // indirect
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 )
